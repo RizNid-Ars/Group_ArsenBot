ea3eea426422411b84862a8d24ff9a04a9a2119b
RizNid-Ars <arsencortombaev@gmail.com>
2024-09-19 21:04:16 +0600

    Add files via upload

---
+++ ArsenHomeworkBot/config.py
@@ -0,0 +1,8 @@
+from aiogram import Bot, Dispatcher
+from decouple import config
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+token = config('TOKEN')
+storage = MemoryStorage()
+bot = Bot(token=token)
+dp = Dispatcher(bot=bot, storage=storage)
