a50cc90662ce3d13885b59e5c54cb20b6f0ce690
RizNid-Ars <arsencortombaev@gmail.com>
2024-09-23 18:40:48 +0600

    Delete ArsenHomeworkBot directory

--- ArsenHomeworkBot/config.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Bot, Dispatcher
-from decouple import config
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-token = config('TOKEN')
-storage = MemoryStorage()
-bot = Bot(token=token)
-dp = Dispatcher(bot=bot, storage=storage)
