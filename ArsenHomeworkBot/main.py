81a45f5b8d79e0b2497b0038591c716d84a8df04
RizNid-Ars <arsencortombaev@gmail.com>
2024-09-23 18:41:34 +0600

    Add files via upload

---
+++ ArsenHomeworkBot/main.py
@@ -0,0 +1,23 @@
+from aiogram import executor
+from config import bot, dp
+from handlers import (start, commands, quiz, game, FSM_store, FSM_reg, webapp, admin_group,
+                      echo)
+
+
+start.register_start(dp=dp)
+commands.register_commands(dp=dp)
+quiz.register_quiz(dp=dp)
+game.register_game(dp=dp)
+FSM_store.register_store(dp=dp)
+FSM_reg.register_fsm_reg(dp=dp)
+admin_group.register_admin_group(dp=dp)
+webapp.register_handlers_webapp(dp=dp)
+
+
+
+echo.register_echo(dp=dp)
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
