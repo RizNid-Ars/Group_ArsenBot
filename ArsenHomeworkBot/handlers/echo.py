ecc8754c79b2ce2726bd839709fde247721079a7
RizNid-Ars <arsencortombaev@gmail.com>
2024-09-27 12:35:52 +0600

    Delete ArsenHomeworkBot directory

--- ArsenHomeworkBot/handlers/echo.py
+++
@@ -1,9 +0,0 @@
-from aiogram import types, Dispatcher
-
-
-async def echo_handler(message: types.Message):
-    await message.answer(text="Неправильная команда!")
-
-
-def register_echo(dp: Dispatcher):
-    dp.register_message_handler(echo_handler)
